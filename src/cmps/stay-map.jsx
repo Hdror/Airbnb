ceccc8650acc7c6340fd73a9b8821acdbfd93318
Yuval Shafrir <10yuval10@gmail.com>
2022-01-19 23:05:45 +0200

    map api

--- src/cmps/stay-map.jsx
+++ src/cmps/stay-map.jsx
@@ -10,61 +10,42 @@ const containerStyle = {
     width: '60vw',
     height: '75vh',
 }
-const centers = [
+const centers = [ // from prev
     {
         lat: 31.777373,
         lng: 34.64148,
-    },
-    {
-        lat: 32.049062,
-        lng: 34.80576,
-    },
-    {
-        lat: 32.838179,
-        lng: 35.08731,
-    },
+    }
 ]
-export function StayMap() {
-    const [center, setCenter] = useState(0)
+
+// console.log(this.state);
+
+export function StayMap(props) {
+    const loc = { lat: props.loc.lat, lng: props.loc.lng }
+    console.log(loc);
+    // const [center, setCenter] = useState(0)
     const [isInfoWindowOpen, setInfo] = useState(false)
-    let branch = ''
-    if (center === 0) branch = 'Yotam ha-Melekh ST. Ashdod'
-    else if (center === 1) branch = 'Mahal ST. 38, Tel-Aviv'
-    else branch = 'Ha-Palmakh ST, Haifa'
+    // let branch = ''
+    // if (center === 0) branch = 'Yotam ha-Melekh ST. Ashdod' // from prev
+    // else if (center === 1) branch = 'Mahal ST. 38, Tel-Aviv'// from prev
+    // else branch = 'Ha-Palmakh ST, Haifa'// from prev
     return (
         <section className='map-section'>
             <div>
                 <LoadScript googleMapsApiKey='AIzaSyCzuZp75Yeu8Eh4TD1RtRYO3Cxs-R5wNwo'>
                     <GoogleMap
                         mapContainerStyle={containerStyle}
-                        center={centers[center]}
+                        center={loc}
                         zoom={14}
                     >
                         <Marker
-                            onClick={() => {
-                                setCenter(0)
-                                setInfo(!isInfoWindowOpen)
-                            }}
+                            // onClick={() => {
+                            //     setCenter(0)
+                            //     setInfo(!isInfoWindowOpen)
+                            // }}
                             name={'Current location'}
-                            position={centers[0]}
+                            position={loc}
                         />
-                        <Marker
-                            onClick={() => {
-                                setCenter(1)
-                                setInfo(!isInfoWindowOpen)
-                            }}
-                            name={'Current location'}
-                            position={centers[1]}
-                        />
-                        <Marker
-                            onClick={() => {
-                                setCenter(2)
-                                setInfo(!isInfoWindowOpen)
-                            }}
-                            name={'Current location'}
-                            position={centers[2]}
-                        />
-                        <InfoWindow position={centers[center]}>
+                        <InfoWindow position={loc}>
                             <h1> Exact location provided after booking </h1>
                         </InfoWindow>
 
