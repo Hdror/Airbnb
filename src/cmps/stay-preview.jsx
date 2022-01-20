7560bc40ce7dc11f47a1ddfaa51ca2ac2a86565f
Dror Handler <hdror507@gmail.com>
2022-01-20 13:10:37 +0200

    style setup

--- src/cmps/stay-preview.jsx
+++ src/cmps/stay-preview.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
 export class StayPreview extends React.Component {
     render() {
         const { _id, name, imgUrls, avgRate, price, address, reviews } = this.props.stay
         return (
-            <Link to={`/stay/${_id}`}>
+            <Link className="clean-link" to={`/stay/${_id}`}>
                 {/* <React.Fragment> */}
                 <div className='stay-preview'>
                     <img src={imgUrls[0]} alt="" />
-                    <span>{avgRate}</span>
-                    <button>{reviews.length}</button>
+                     <span>{avgRate}</span>
+                    <button>{reviews.length}reviews</button>
                     <h3>{address}</h3>
                     <h1>{name}</h1>
-                    <h2>{price} / night</h2>
+                    <h2>{price}$ / night</h2>
                 </div>
                 {/* </React.Fragment> */}
             </Link>
