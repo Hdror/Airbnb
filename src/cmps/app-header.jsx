340179267b2d88765a0b7d075069cebd256d1188
Ori Cohen <cori20789@gmail.com>
2022-01-21 14:30:04 +0200

    header

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link, NavLink } from 'react-router-dom'
 
-import { loadStays } from '../store/stay.action.js'
+// import { loadStays } from '../store/stay.action.js'
+
+// SVG
+import menu from '../assest/svg/app-header/menu.svg'
 // import { DateRange } from './date-range.jsx'
 
 class _AppHeader extends React.Component {
@@ -59,19 +62,77 @@ class _AppHeader extends React.Component {
 
     render() {
         return (
-            <section>
-                <header>
-                    <div><Link to="/" className="logo">LOGO</Link></div>
-                    <div>
+            <header className="main-container">
+                <div className="main-header flex">
+                    <Link to="/" className="logo">LOGO</Link>
+                    <div className="menu-actions flex">
                         <NavLink className="clean-link" to="/stay">Explore</NavLink>
-                        <NavLink className="clean-link" to="/stay">Become a host</NavLink>
+                        <NavLink className="clean-link" to="/host">Become a host</NavLink>
+                        <div className="menu-wrapper flex">
+                            <img className="menu-icon" src={menu} alt="" />
+                            <img src="https://randomuser.me/api/portraits/women/95.jpg" alt="" />
+                        </div>
                     </div>
-                    <div>
-                        <span>SVG</span>
-                        <img src="https://randomuser.me/api/portraits/women/95.jpg" alt="" />
-                    </div>
-                </header>
-            </section>
+                </div>
+                <div className="trip-build-container flex">
+                    <form action="">
+                        <div>
+                            <div>
+                                <div className="flex">
+                                    <div className="flex">
+                                        <label htmlFor="trip-location">
+                                            <div className="trip-selections">Location</div>
+                                            <input className="search-input trip-instructions"
+                                                type="text"
+                                                aria-autocomplete="none"
+                                                autoCorrect="off"
+                                                name="quary"
+                                                placeholder="Where are  you going?"
+                                            />
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div>
+                                <div className="flex">
+                                    <div className="flex">
+                                        <div className="trip-selections">Check in</div>
+                                        <div className="trip-instructions">Add dates</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="flex">
+                                    <div className="flex">
+                                        <div className="trip-selections">Check out</div>
+                                        <div className="trip-instructions">Add dates</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex">
+                                <div className="flex">
+                                    <div className="trip-selections">Guests</div>
+                                    <div className="trip-instructions">Add guests</div>
+                                </div>
+                            </div>
+                            <div className="flex">
+                                <button>
+                                    <div>
+                                        <div><img src="https://iconpacks.net/?utm_source=link-attribution&utm_content=2906" alt="" /> </div>
+                                        <div>Search</div>
+                                    </div>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </header>
         )
     }
 }
