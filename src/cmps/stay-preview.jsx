d9a0835e3aecd989262a2dd01dc38ac7bb76dc51
Ori Cohen <cori20789@gmail.com>
2022-01-30 13:28:51 +0200

    bug

--- src/cmps/stay-preview.jsx
+++ src/cmps/stay-preview.jsx
@@ -18,9 +18,9 @@ export class StayPreview extends React.Component {
                     <div className="stay-preview-info">
                         <div className="flex align-center">
                             {/* <img src={Star} alt="not-found" className="star" /> <span>{avgRate}</span> */}
-                            <img src={Star} alt="not-found" className="star" /> <span>{(utilService.getRandomIntInclusive(3,4)+Math.random()).toFixed(2)}</span>
+                            <img src={Star} alt="not-found" className="star" /><span>{(utilService.getRandomIntInclusive(3,4)+Math.random()).toFixed(2)}</span>
                             {/* <span className="review-count" >( {!reviews.length ? 'No reviews' : reviews.length} ) </span> */}
-                            <span className="review-count" >( {utilService.getRandomIntInclusive(20,50)}) </span>
+                            <span className="review-count" >({utilService.getRandomIntInclusive(20,50)}) </span>
                         </div>
                         <h3>{typeOfPlace} · {loc.address}</h3>
                         <h3>{name}</h3>
