1f423aa74d4e4a0541198de889814c6b2e2a9147
Yuval Shafrir <10yuval10@gmail.com>
2022-03-10 14:09:55 +0200

    design stay add page

--- src/cmps/upload.jsx
+++ src/cmps/upload.jsx
@@ -3,7 +3,7 @@ import { cloudinaryService } from '../services/cloudinary.service.js'
 
 export class Upload extends Component {
     state = {
-        imgUrl: null,
+        imgUrl: this.props.imgUrl,
         height: 60,
         width: 60,
         isUploading: false
@@ -14,12 +14,27 @@ export class Upload extends Component {
         if (userImgUrl) this.setState({ imgUrl: userImgUrl })
     }
 
+    componentDidUpdate(prevProps) {
+        // if (!this.props.userImgUrl && this.state.imgUrl !== '') this.setState({ imgUrl: '' })
+        if (this.props !== prevProps) this.setState({ imgUrl: this.props.userImgUrl })
+    }
+
+    resetUploads = () => {
+        console.log('reset');
+        this.setState({
+            imgUrl: null,
+            height: 60,
+            width: 60,
+            isUploading: false
+        })
+    }
+
     uploadImg = async (ev) => {
         this.setState({ isUploading: true })
         const { secure_url, height, width } = await cloudinaryService.uploadImg(ev)
         this.setState({ isUploading: false, imgUrl: secure_url, height, width }, () => { this.props.onUploadImg(this.state, this.props.position) })
     }
-    
+
     get uploadMsg() {
         const { imgUrl, isUploading } = this.state
         if (imgUrl) return 'Upload Another?'
@@ -27,16 +42,19 @@ export class Upload extends Component {
     }
 
     render() {
+        console.log(this.props);
         const { imgUrl } = this.state
         const previewStyle = {
             backgroundImage: `url(${imgUrl})`,
-
         }
+
         return (
-            <div className="upload-preview" style={previewStyle} >
-                <label className="img-upload-label" htmlFor="imgUpload">{this.uploadMsg}
+            <div className="upload-preview">
+                <div className="img-upload-label" htmlFor="imgUpload">
+                    {/* {this.uploadMsg} */}
                     <input type="file" onChange={this.uploadImg} accept="img/*" className="img-upload-btn" id="imgUpload" />
-                </label>
+                    <img src={imgUrl} alt="" />
+                </div>
             </div>
         )
     }
